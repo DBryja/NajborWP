95285ce8e8afc7618b3696564c8ac1b3b75b6e71
Dawid Bryja <dawbryja@gmail.com>
2025-03-07 14:12:05 +0100

    Copyrights date function

--- wp-content/themes/najbor/template-parts/content-copyrights.php
+++ wp-content/themes/najbor/template-parts/content-copyrights.php
@@ -1,5 +1,11 @@
 <?php
 $lang = get_site_language();
 $realisation = ml_realisation();
+$startYear = 2025;
+$currentYear = date('Y');
+$dateToShow = $startYear;
+if ($startYear != $currentYear) {
+    $dateToShow = $startYear.'-'.$currentYear;
+}
 ?>
-<span class="copyrights">© Wiktor Najbor 2024 | <?php echo $realisation[$lang]?>:&nbsp;<a href="https://github.com/DBryja" target="_blank" rel="noreferrer">Dawid Bryja</a></span>
+<span class="copyrights">© Wiktor Najbor <?php echo $dateToShow ?> | <?php echo $realisation[$lang]?>:&nbsp;<a href="https://github.com/DBryja" target="_blank" rel="noreferrer">Dawid Bryja</a></span>
